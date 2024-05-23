922778fe3060f83663ae68af4db4bf36c6a8b365
Ancishan <shantodev027@gmail.com>
2024-05-23 21:42:58 +0600

    [DONE]  user and jwt and admin  fucntionailty

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -26,9 +27,102 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
 
 
+    const userCollection = client.db("bistroDb").collection('users');
     const menuCollection = client.db("bistroDb").collection('menu');
     const reviewCollection = client.db("bistroDb").collection('reviews');
     const cartCollection = client.db("bistroDb").collection('carts');
+
+
+    // jwt related api
+    app.post('/jwt', async(req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({token});
+    })
+
+    //middleWares
+    const verifyToken = (req, res, next) =>{
+      console.log('inside verify token',req.headers);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: "unAuthorized access"});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token,  process.env.ACCESS_TOKEN_SECRET, (error, decoded) =>{
+        if(error){
+          return res.status(401).send({message: 'unAuthorized access'})
+        }
+        req.decoded= decoded;
+        next();
+      } )
+    } 
+
+    //use verify Admin after verify token
+    const verifyAdmin = async(req, res, next) =>{
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      next();
+    }
+
+    // user related api
+// database theke get kora user
+    app.get('/users',verifyToken,verifyAdmin, async(req, res) =>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+
+    // users k admin krr jnno
+    app.patch('/users/admin/:id',verifyToken,verifyAdmin, async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc)
+      res.send(result);
+    })
+
+    app.get('/user/admin/:email', verifyToken, async(req, res) =>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'unauthorized access'})
+      }
+      const query = { email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({admin});
+    })
+
+    // user k admin delete kore dbe
+    app.delete('/users/:id', verifyToken,verifyAdmin, async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.post('/users', async(req,res) =>{
+      const user = req.body;
+      // insert email if user doesnt exists
+      // i can do this many ways:(email unique, upsert ,simple)
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already existing', insertedId: null})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
 
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
@@ -54,6 +148,14 @@ async function run() {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem)
       res.send(result)
+    })
+
+    // cart delete
+    app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
    
     // Send a ping to confirm a successful connection
